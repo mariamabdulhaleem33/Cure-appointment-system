e4bed6e1c3ca1b451cd291337116f9cf85a4b338
AhmedEhabHM <ahmed50ehab@gmail.com>
2025-12-20 20:28:06 +0200

    Add Payment page & Rating page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import MainLayout from "./components/layout/MainLayout"
-
+import MainLayout from "./components/layout/MainLayout";
 
 const App = () => {
   return (
     <div className="min-h-screen px-8 py-4 bg-white">
       <MainLayout />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
