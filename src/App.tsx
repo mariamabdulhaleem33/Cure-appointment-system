7ae9c07486e02de562a0d7246f0b8d9f7eb46472
mariamabdulhaleem <mariamabdulhaleem186@gmail.com>
2025-12-23 15:24:49 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import AppRoutes from "./routes/index";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </Provider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
