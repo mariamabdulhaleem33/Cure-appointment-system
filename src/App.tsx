da6fcbc76e245d3d13244faa82119f3b02d4318c
20Hanan <hanansayed2002@gmail.com>
2025-12-24 15:51:47 +0200

    Remove unused Flowbite React files and components; refactor YourAppointments page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 
-import { BrowserRouter } from "react-router-dom"
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import AppRoutes from './routes/index'  
+// import { BrowserRouter } from "react-router-dom"
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import AppRoutes from './routes/index'  
 
-const queryClient = new QueryClient()
+import YourAppointments from "./components/appointments-page/YourAppointments"
+// import ReviewCards from "./components/create_booking/ReviewCards"
+import ReviewCard from "./components/rating/ReviewCard"
+
+// const queryClient = new QueryClient()
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AppRoutes />  
-      </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    // <QueryClientProvider client={queryClient}>
+    //   <BrowserRouter>
+    //     <AppRoutes />  
+    //   </BrowserRouter>
+    //   <ReactQueryDevtools initialIsOpen={false} />
+    // </QueryClientProvider>
+    <>
+    <YourAppointments/>
+    {/* <ReviewCard/> */}
+    {/* <ReviewCards/> */}
+    </>
   )
 }
 
