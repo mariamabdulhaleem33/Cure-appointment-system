cc088af772fe33074db235a859a18cec9f2b1847
shaima-metwaly <shimaametwaly331@gmail.com>
2025-12-26 18:08:43 +0300

    update styles for  sign in page

--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -5,8 +5,8 @@ import Logo from "@/components/layout/navbar/Logo"
 
 const SignUp = () => {
   return (
-    <div className=" flex min-h-screen  items-center justify-center lg:justify-around">
-      <div className="fixed top-9.5 left-25 bg-white ">
+    <div className=" flex flex-col lg:flex-row min-h-screen  items-center justify-center lg:justify-around">
+      <div className="relative lg:fixed top-9.5 left-6 lg:left-25 bg-white self-start">
         <Logo />
       </div>
       <div><SignUpForm /></div>
